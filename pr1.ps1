9f69c0302fdffff9abb7b875c74e37e37d7e0681
Egert Joonas <egert.joonas@voco.ee>
2025-02-10 14:26:05 +0000

    ArrayList

---
+++ pr1.ps1
@@ -0,0 +1,14 @@
+﻿# two arrays
+$a1 = @(1,2,3)
+$a1 = @(4,5,6)
+
+# empty array
+$a3 = @()
+
+# adds values to the array
+$a3 += $a1[0] + $a2[0]
+$a3 += $a1[1] + $a2[1]
+$a3 += $a1[2] + $a2[2]
+
+#prints third array
+$a3
