db74af5c6208578eec5845edc51f3720142c6a69
Egert Joonas <egert.joonas@voco.ee>
2025-02-10 15:59:56 +0000

    Ylesanne 10

---
+++ pr10.ps1
@@ -0,0 +1,10 @@
+﻿$count = 0
+
+do {
+    Write-Host 'Notepad is running'
+    $count++
+    Start-Sleep -Seconds 1
+    $Notepad = Get-Process notepad -ErrorAction SilentlyContinue
+} until ($Notepad -eq $null )
+
+Write-Host $count
