f1ea367b18bf8a4958b6195003cb44475f3b5b8e
Egert Joonas <egert.joonas@voco.ee>
2025-02-10 15:52:01 +0000

    Ylesanne 7

---
+++ pr7.ps1
@@ -0,0 +1,16 @@
+﻿#värvide nimekiri
+$grupp = @("Red", "Yellow", "Blue", "Green")
+
+$end=@()
+
+for($i = 1; $i -le 20; $i++){
+    $g = Get-Random $grupp
+    $temp = [PSCustomObject]@{
+        RollNumber = $i
+        Color  = $g
+    }
+    $end += $temp
+}
+
+$end
+   
