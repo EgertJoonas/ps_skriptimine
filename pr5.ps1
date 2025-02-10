2ed0391aeef81a2d63506821295ff135e3f8e04b
Egert Joonas <egert.joonas@voco.ee>
2025-02-10 14:59:28 +0000

    Ylesanne 5

---
+++ pr5.ps1
@@ -0,0 +1,16 @@
+﻿#Esimene osa
+
+Start-Process "notepad.exe"
+
+# Leiab kõik notepadi protsessid ja näitab nime ja id
+Get-Process notepad | Select-Object Name, Id
+
+#Teine osa
+
+Get-ChildItem C:\temp\test
+
+$file = Get-ChildItem C:\temp\test | where{$_.Name -like "*.csv"} | select Name, Length
+$sizeKB = $file.Length/1KB
+$sizeMB = $file.Length/1MB
+
+Write-Host "`nFilename : " $file.Name "`nSize in KB : " $sizeKB "`nSize in MB : " $sizeMB
